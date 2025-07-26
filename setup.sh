5d90d36f4993ede2444f87c77d2a240d61be9200
WilliamS <73660888+cnwyshi@users.noreply.github.com>
2025-07-25 22:09:51 -0700

    image caption

---
+++ setup.sh
@@ -0,0 +1,10 @@
+pip3 install virtualenv 
+virtualenv my_env # create a virtual environment my_env
+source my_env/bin/activate # activate my_env
+
+pip install langchain==0.1.11 gradio==5.23.2 transformers==4.38.2 bs4==0.0.2 requests==2.31.0 torch==2.2.1
+
+source my_env/bin/activate # activate my_env
+
+python3 image_cap.py 
+
